01bc7078c29c42db0146307460c7a86f60fd3097
Alperen <55198825+alperensa@users.noreply.github.com>
2024-05-19 15:25:57 +0300

    Delete src/constants/api.js

--- src/constants/api.js
+++
@@ -1,8 +0,0 @@
-export const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Zjc0YjA0ZTVkZjU0OWU2ZTI1MTE5OTY3NzI0OTVkZCIsInN1YiI6IjY2M2I2MzYwZGMyZmUyMmZlNjhlYjJhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NYzSRvKhJhYV-WL2WgYnWcRMgQrHZuwYxJ-6I4LLx8I",
-    },
-  };
